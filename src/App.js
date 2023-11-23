d8b28c4860f58a31ec7c1cec268170f833741a74
VW <fds.vctr@gmail.com>
2023-11-23 22:02:39 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+import { useState } from "react";
+import "./App.css";
 function App() {
+  const [text, setText] = useState("");
+
+  const sendMessage = () => {
+    console.log(text);
+    // тут пишетвсся логика для отправки на бэк
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="green_text">{text}</h1>
+      <input
+        type="text"
+        value={text}
+        placeholder="WriteHere"
+        onChange={(event) => setText(event.target.value)}
+      />
+      <button onClick={sendMessage}>send message</button>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
