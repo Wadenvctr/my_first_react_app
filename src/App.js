322d1597263f80082c2475900a6d51b01deb840b
VW <fds.vctr@gmail.com>
2023-12-06 23:23:27 +0300

    add smth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-// import logo from './logo.svg';
-//import "./App.css";
 import { useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 function App() {
   const [posts, setPosts] = useState([
     {
@@ -15,44 +13,48 @@ function App() {
     { id: 2, title: "Javascript2", body: "тут тоже" },
     { id: 3, title: "Javascript3", body: "и даже тут" },
   ]);
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
 
-  const addNewPost = (e) => {
-    e.preventDefault()
-    const newPost = {
-      id: Date.now(),
-      title,
-      body
-    }
-    setPosts([...posts, newPost])
-    console.log(posts)
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
   };
+
+  //берем пост из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const [selectedSort, setSelectedSort] = useState()
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+  }
 
   return (
     <div className="App">
-      <form>
-        {/* управляемыe компоненты */}
-        <MyInput
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          type="text"
-          placeholder="Name of post"
+      <PostForm create={createPost} />
+      <hr style={{margin: '15px'}}/>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue='Сортировка'
+          options={[
+            {value: 'title', name:'по названию'},
+            {value: 'body', name:'по описанию'},
+          ]}
         />
-        <MyInput 
-        value={body}
-        onChange={(e) => setBody(e.target.value)}
-        type="text" 
-        placeholder="description of post" 
+      </div>
+      {posts.length !== 0 
+      ?
+        <PostList
+          remove={removePost}
+          posts={posts}
+          title="Список постов, текст меняется динамически из App.js"
         />
-        <MyButton type="submit" onClick={addNewPost}>
-          Создать пост
-        </MyButton>
-      </form>
-      <PostList
-        posts={posts}
-        title={"Список постов, текст меняется динамически из App.js"}
-      />
+      :
+        <h1 style={{ textAlign: "center" }}>Посты не найдены</h1>
+      }
     </div>
   );
 }
