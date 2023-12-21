1b036d2c3aa3af8e68c6e4681379c2ff08ca5bdc
VW <fds.vctr@gmail.com>
2023-12-21 16:44:36 +0300

    v.03 1:53

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,47 +6,60 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./components/hooks/usePosts";
-import axios from "axios";
+import PostService from "./components/API/PostService";
+import Loader from "./components/UI/loader/Loader";
+import { useFetching } from "./components/hooks/useFetching";
 function App() {
+  
   const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({
+    sort: "",
+    query: "",
+  });
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll()
+    setPosts(posts)
+  })
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false)
   };
 
-  async function fetchPosts() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    setPosts(response.data)
-  }
 
   //берем пост из дочернего компонента
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
-  const [filter, setFilter] = useState({
-    sort: "",
-    query: "",
-  });
-  const [modal, setModal] = useState(false)
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   return (
     <div className="App">
-      <MyButton onClick={fetchPosts}>GET POSTS</MyButton>
       <MyButton style={{marginTop: '30px', }} onClick={() => setModal(true)}>
-        Создать пользователя
+        Создать пост
       </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
       <PostForm create={createPost} />
       </MyModal>
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
+      {postError &&
+        <h1>Произошла ошибка : {postError}</h1>
+      }
+      {isPostsLoading
+        ?
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+        : 
+        <PostList
         remove={removePost}
         posts={sortedAndSearchedPosts}
         title="Список постов, текст меняется динамически из App.js"
       />
+      }
     </div>
   );
 }
--- src/components/hooks/usePosts.js
+++ src/components/hooks/usePosts.js
@@ -1,7 +1,6 @@
 import {useMemo} from "react";
 export const useSortedPosts = (posts, sort) => {
     const sortedPosts = useMemo(() => {
-        console.log("Отработал хук useMemo");
         if (sort) {
           return [...posts].sort((a, b) =>
             a[sort].localeCompare(b[sort])
